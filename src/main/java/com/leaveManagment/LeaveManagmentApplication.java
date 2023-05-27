945cd677927e24324909b3b7779905443af44158
Mootaz-Mellouli <mootazmellouli@hotmail.com>
2023-05-27 11:11:20 +0100

    Initial commit

---
+++ src/main/java/com/leaveManagment/LeaveManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.leaveManagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaveManagmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leaveManagment/LeaveManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leaveManagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaveManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
