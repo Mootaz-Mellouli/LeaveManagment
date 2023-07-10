2be8cbdeb3422bed068e534979d3470acffb175e
Saif-Eddine Hazemi <saif-eddine.hazemi@inetum.com>
2023-07-10 20:08:06 +0100

    Commit le 10/07/2023

--- src/main/java/com/leaveManagment/controllers/UserController.java
+++ src/main/java/com/leaveManagment/controllers/UserController.java
@@ -4,7 +4,7 @@ import com.leaveManagment.entities.Leave;
 import com.leaveManagment.entities.Team;
 import com.leaveManagment.entities.User;
 import com.leaveManagment.services.Team.ITeamService;
-import com.leaveManagment.services.User.IUserService;
+import com.leaveManagment.services.User.UserServiceImp;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,7 +15,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
-    private final IUserService userService;
+    private final UserServiceImp userService;
     @GetMapping()
     public List<User> retrieveAllUsers (){
         return userService.retrieveAllUsers();
